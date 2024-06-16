ab8ac432dbd9cbfe7e8fc80fbd1cb3f8181cf1bb
Savion Goslee <saviongoslee@gmail.com>
2024-06-16 15:21:13 -0700

    Added an interpolation example.

---
+++ interpolation.py
@@ -0,0 +1,46 @@
+# Example of Interpolation Using NumPy.
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Configuration
+
+x = np.array([5, 4, 9])
+y = np.array([6, 2, 8])
+
+use_graph = True
+
+# Functions
+
+def use_poly(poly, x_range): # Adjust x-range, calculate y-values using polyfit.
+    new_x = np.array(x_range)
+    new_y = poly(new_x)
+    return new_x, new_y
+
+
+def show_graph(x, y, new_x, new_y):
+    if not use_graph: return
+
+    plt.plot(new_x, new_y, label='Polynomial Fit')
+    plt.scatter(x, y, color='red', label='Data Points')
+    plt.xlabel('X-axis')
+    plt.ylabel('Y-axis')
+    plt.title('Polynomial Interpolation')
+    plt.legend()
+    plt.show()
+
+# Program
+        
+def main():
+    curve = np.polyfit(x, y, 2) # Fits a polynomial using a degree to (x, y).
+    poly = np.poly1d(curve) # Create a Polynomial object using the fit.
+    new_x, new_y = use_poly(poly, range(-10, 12))
+    
+    print(f"""
+    Polynomial Fit:
+    {poly}
+    """)
+
+    show_graph(x, y, new_x, new_y)    
+
+main()
